7fffbc0da047a6ce2015e4a7754f4249b68e0ca5
isaac <isaacnotnwton@gmail.com>
2014-07-03 18:20:37 +0800

    push again first map show on foreman

--- hello.py
+++ hello.py
@@ -1,21 +1,12 @@
 from flask import Flask, render_template, request
 from sqlalchemy import *
 import simplejson as json
-<<<<<<< HEAD
 import collections
 import requests
 
 import os 
 import psycopg2
 # import urlparse
-=======
-# import collections
-# import requests
-
-import os 
-import psycopg2
-#import urlparse
->>>>>>> 464b7a38bbe19354749658629904712446153c0d
 
 
 app = Flask(__name__)
@@ -35,11 +26,7 @@ def hello():
 
 	
 	#url = urlparse.urlparse(os.environ["postgres://pkjzpvyckjdzbl:GbH_pvSv1GhOIQC5VRD1NKkmUb@ec2-174-129-218-200.compute-1.amazonaws.com:5432/d64hrkgakrskqs"])
-<<<<<<< HEAD
 	# urlparse.uses_netloc.append("postgres")
-=======
-	#urlparse.uses_netloc.append("postgres")
->>>>>>> 464b7a38bbe19354749658629904712446153c0d
 
 	##url = urlparse.urlparse("postgres://gqlskkqipzmtai:1tuYJio5GMTI7-iWpZ6YlzgHH_@ec2-54-228-195-37.eu-west-1.compute.amazonaws.com:5432/d4ej7n7dsh1s1n")
 	## url = urlparse.urlparse(os.environ["DATABASE_URL"])
@@ -54,17 +41,10 @@ def hello():
 
 	cur = conn.cursor()
 
-<<<<<<< HEAD
 	cur.execute("SELECT longitude, latitude, project_name, commodity_name, country_code FROM map_resources_table WHERE latitude != 0")
 
 	# print "Connect Successfully. Some records from test site\n", cur.fetchall()
 	# return cur.fetchall()[0]
-=======
-	cur.execute("SELECT * FROM map_resources_table WHERE latitude!=0")
-
-	# print "Connect Successfully. Some records from test site\n", cur.fetchall()
-	#return cur.fetchall()[0]
->>>>>>> 464b7a38bbe19354749658629904712446153c0d
 
 	# db = create_engine('postgresql://postgres:cloudminer@localhost:5432/postgres')
 	# # db = create_engine('postgres://gqlskkqipzmtai:1tuYJio5GMTI7-iWpZ6YlzgHH_@ec2-54-228-195-37.eu-west-1.compute.amazonaws.com:5432/d4ej7n7dsh1s1n')
@@ -76,14 +56,9 @@ def hello():
 	# s = users.select(not_(users.c.latitude == 0))
 
 	# rs = s.execute()
-<<<<<<< HEAD
 	# rows = rs.fetchmany(5)
 	rows = cur.fetchmany(5)
 	# return rows[0]
-=======
-	rows = cur.fetchmany(5)
-
->>>>>>> 464b7a38bbe19354749658629904712446153c0d
 	# return "lol"
 	
 	#196880
@@ -91,7 +66,6 @@ def hello():
 	# #-171.6199
 
 	rowarray_list = []
-<<<<<<< HEAD
 	# print rows
 	#filename = request.form["commodity"]
 	# count = 0
@@ -124,28 +98,6 @@ def hello():
 		# t = {'lon': row1.longitude, 'lat': row1.latitude, 'project': row1.project_name, 'number': row1.id}
 			rowarray_list.append(t)
 			# count = 0
-=======
-
-	# #filename = request.form["commodity"]
-	# count = 0
-	for row in rows:
-
-		# if filename in row1.commodity_name:
-			geometry = {'coordinates': [row.longitude, row.latitude], 'type': "Point"}
-			# properties = {'publicid': row.project_name, 'origintime': row.project_name, 'longitude': row.longitude, 'latitude': row.latitude, 'depth': row.id, 
-			# 'magnitude': row.id, 'magnitudetype': row.project_name, 'status': row.project_name, 'phases': row.id, 'type': row.project_name, 
-			# 'agency': row.project_name, 'updatetime': row.project_name, 'bbox': [row.longitude, row.latitude, row.longitude, row.latitude]}
-			properties = {}
-			t = {'type': "Feature", 'geometry': geometry, 'geometry_name': row.project_name, 'properties': properties, 'commodity': row.commodity_name, 'country_code': row.country_code}
-		# for row2 in rows:
-
-		# 	if (row1.latitude == row2.latitude) and (row1.longitude == row2.longitude):
-		# 		count = count + 1
-
-		# t = {'lon': row1.longitude, 'lat': row1.latitude, 'project': row1.project_name, 'number': row1.id}
-			rowarray_list.append(t)
-			count = 0
->>>>>>> 464b7a38bbe19354749658629904712446153c0d
 
 
 			# if filename in row1.commodity_name:
@@ -165,11 +117,7 @@ def hello():
    	j = json.dumps(rowarray_list,  use_decimal=True, sort_keys = True)
 
    	j = '{"type": "FeatureCollection", "features":' + j + ', "crs":{"type":"EPSG","properties":{"code":"4326"}},"bbox":[-1000, 1000, -1000, -1000]}'
-<<<<<<< HEAD
    	# print j[-1000:-1]
-=======
-   	print j[-1000:-1]
->>>>>>> 464b7a38bbe19354749658629904712446153c0d
    	#j = '{"type": "", "features":' + j + ', "crs": {}, "bbox": []}'
    	#j = '{"objects":' + j + '}'
    	f = open('static/data/data.json', 'w+')
@@ -182,8 +130,4 @@ def hello():
 
 
 # if __name__ == "__main__":
-<<<<<<< HEAD
 #     app.run(debug=True)
-=======
-#     app.run(debug=True)
->>>>>>> 464b7a38bbe19354749658629904712446153c0d
